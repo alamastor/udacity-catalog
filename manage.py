ac89abe367aa372a05a917b135d8665c9462cb61
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-11-02 17:10:07 +1100

    feat: Db populate function

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ from flask.ext.script import Manager, Server
 
 from app import app
 from app.db import db
+from app.models.item import Item
+from app.models.catagory import Catagory
+
 
 manager = Manager(app)
 
@@ -17,8 +20,35 @@ def create_tables():
 
 
 @manager.command
+def populate():
+    items = [
+        ('ball', 'soccer'),
+        ('stick', 'basketball'),
+        ('goggles', 'soccer'),
+        ('snowboard', 'soccer'),
+        ('shinguards', 'frisbee'),
+        ('frisbee', 'soccer'),
+        ('bat', 'soccer'),
+        ('raquette', 'soccer'),
+        ('helmet', 'soccer'),
+        ('gloves', 'soccer'),
+        ('net', 'soccer'),
+        ('ball', 'basketball'),
+        ('disk', 'soccer'),
+        ('boots', 'soccer'),
+        ('sock', 'soccer'),
+    ]
+    for item in items:
+        if Catagory.exists(item[1]):
+            Item.create(item[0], catagory_name=item[1])
+        else:
+            catagory = Catagory.create(name=item[1])
+            Item.create(item[0], catagory=catagory)
+
+
+@manager.command
 def drop_tables():
-    db.drop_tables()
+    db.drop_all()
 
 
 if __name__ == '__main__':
