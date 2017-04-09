1d8f2e9b4e0aa34df906a82bbadf8b2023415ada
Alistair McKelvie <alistair.mckelvie@gmail.com>
2017-04-09 23:43:10 +1000

    fix: Error code returned by handlers

--- app/views/errors.py
+++ app/views/errors.py
@@ -13,16 +13,16 @@ def not_found(e):
 @app.errorhandler(401)
 def unauthorized(e):
     error_text = '401: Unauthorized.'
-    return render_template('error.html', error_text=error_text), 404
+    return render_template('error.html', error_text=error_text), 401
 
 
 @app.errorhandler(403)
 def forbidden(e):
     error_text = '403: Forbidden.'
-    return render_template('error.html', error_text=error_text), 404
+    return render_template('error.html', error_text=error_text), 403
 
 
 @app.errorhandler(500)
 def error(e):
     error_text = '500: Server error.'
-    return render_template('error.html', error_text=error_text), 404
+    return render_template('error.html', error_text=error_text), 500
